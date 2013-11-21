22249cd84e35d564ee9c078da02590970f363163
Subwai <adam.h.lyren@gmail.com>
2013-11-21 14:23:29 +0100

    changed db name

--- Application/Controller/Component/Edit_calendarController.php
+++ Application/Controller/Component/Edit_calendarController.php
@@ -11,7 +11,7 @@ class Edit_calendarController extends ApplicationController {
 
             $stmt = $this->DatabaseMgr->Execute(@"SELECT `series`.`name`, date_format(`series`.`release_time`, '%H:%i') as `release_time`, `series`.`release_week_day`
                                                 FROM `series` INNER JOIN `facebook_calendar` ON `facebook_calendar`.`series` = `series`.`id`
-                                                WHERE `facebook_calendar`.`facebook` = ".FacebookManager::FacebookUser(), MAIN_DB);
+                                                WHERE `facebook_calendar`.`facebook` = ".FacebookManager::FacebookUser(), MAIN);
             $stmt->setFetchMode(PDO::FETCH_CLASS, "SeriesModel");
             $calendar = $stmt->fetchAll();
         }
@@ -25,7 +25,7 @@ class Edit_calendarController extends ApplicationController {
         if (isset($_POST["search_text"])) {
             $stmt = $this->DatabaseMgr->Prepare(@"SELECT name FROM series WHERE
                                                 MATCH (name,description) AGAINST
-                                                (:search IN BOOLEAN MODE)", MAIN_DB);
+                                                (:search IN BOOLEAN MODE)", MAIN);
             $stmt->execute(array("search" => $_POST["search_text"]));
             $results = $stmt->fetchAll();
         }
